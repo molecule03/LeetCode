726caf78e0568492694a55767cc970e623c8db8c
molecule03-zeus <lavesh.salaskar@zeuslearning.com>
2023-07-16 12:45:43 +0530

    Initial Commit

---
+++ W354/A.java
@@ -0,0 +1,31 @@
+package LeetCode_Contest.W354;
+
+import java.util.*;
+
+public class A {
+
+    public static void main(String[] args) {
+
+        int nums[] ={1,3,6,3,1,2};
+        int target = 2;
+        System.out.println(sumOfSquares(nums));
+    }
+
+    //    Corrected Code in Upsolve
+    public static int sumOfSquares(int[] nums) {
+
+        int mod = 1000000007;
+        int n = nums.length;
+
+        long sum = 0;
+
+        for(int i=0; i<n; i++){
+            if(n%(i+1) == 0){
+                sum += (nums[i]*nums[i]);
+            }
+            // System.out.println(sum);
+        }
+
+        return (int)sum%mod;
+    }
+}
---
+++ W354/C.java
@@ -0,0 +1,19 @@
+package LeetCode_Contest.W354;
+
+import java.util.*;
+
+public class C {
+
+    public static void main(String[] args) {
+
+        int nums[] ={1,3,6,3,1,2};
+        int target = 2;
+//        System.out.println(maximumJumps(nums, target));
+    }
+
+    //    Corrected Code in Upsolve
+//    public static int maximumJumps(int[] nums, int target) {
+//
+//
+//    }
+}
